45baeb6353502cef88ad0362c0a16365d7a51331
Robert Anderson <robster31@googlemail.com>
2016-08-01 09:47:09 +0100

    + Added default ss environment to the repo (git ignored)

--- public_html/_ss_environment_LIVE.php
+++ public_html/_ss_environment_LIVE.php
@@ -1,9 +1,9 @@
 <?php
 
-define('SS_DATABASE_SERVER', '10.168.1.84');
-define('SS_DATABASE_USERNAME', 'connecte11_silve');
-define('SS_DATABASE_PASSWORD', 'VZAA44nlteQptyYO');
-define('SS_DATABASE_NAME', 'connecte11_silver');
+define('SS_DATABASE_SERVER', 'w.x.y.z');
+define('SS_DATABASE_USERNAME', 'USERNAME');
+define('SS_DATABASE_PASSWORD', 'PASSWORD');
+define('SS_DATABASE_NAME', 'DB_NAME');
 define('SS_DATABASE_CHOOSE_NAME', 2);
 
 define('SS_ENVIRONMENT_TYPE', 'live');
@@ -17,7 +17,7 @@ ini_set('display_errors', 1);
 //ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']))."/public_html/silverstripe-cache/session");
 
 global $_FILE_TO_URL_MAPPING;
-$_FILE_TO_URL_MAPPING['/var/www/'] = 'http://connected.thalesapp.com';
+$_FILE_TO_URL_MAPPING['/var/www/'] = 'http://myfancysite.com';
 
 if (defined('SS_ENVIRONMENT_TYPE') && SS_ENVIRONMENT_TYPE != 'live') {
     // turn on display_errors if we are in dev
